a976b75e8deebd9483f776fa23bfe7c9d278fc0f
MattScoville <Matt.Scoville+GitHub@gmail.com>
2013-01-31 14:21:09 -0500

    added variables to task.
    Next step will be fully implementing them.
    
    Signed-off-by: MattScoville <Matt.Scoville+GitHub@gmail.com>

--- include/task.h
+++ include/task.h
@@ -1,6 +1,10 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include <list>
+#include <string>
+using namespace std;
+
 
 class Task
 {
@@ -11,6 +15,11 @@ class Task
         virtual ~Task();
     protected:
     private:
+        string str_Task;  // The Actual task
+        int owner_ID;  // The ID of task above this one.
+        int ID;  // The ID of this task
+        bool completed; // If the task is completed
+        list<Task> Subtasks;  // a list of subtasks
 };
 
 #endif // TASK_H
--- src/task.cpp
+++ src/task.cpp
@@ -1,8 +1,18 @@
-#include "task.h"
+#include "..\include\task.h"
+#include <string>
 
 Task::Task()
 {
     //ctor
+      /* These are all the current varibles Some may change or be added later.
+        string str_Task;  // The Actual task
+        int owner_ID;  // The ID of task above this one.
+        int ID;  // The ID of this task
+        bool completed; // If the task is completed
+        list<Task> Subtasks;  // a list of subtasks
+
+        */
+        str_Task = "";
 }
 
 Task::~Task()
